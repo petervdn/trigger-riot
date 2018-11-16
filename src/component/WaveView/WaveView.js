71354d5dfbbc2d790396129f5ab9fe696b4da6a9
Peter van der Noord <peterdunord@gmail.com>
2018-11-16 23:20:37 +0100

    Show correct name for selected waveView

--- src/component/WaveView/WaveView.js
+++ src/component/WaveView/WaveView.js
@@ -38,9 +38,10 @@ export default {
         return `${this.matrixItems[0].position.x + 1}.${this.matrixItems[0].position.y + 1}`;
       }
 
-      const groupType =
-        this.matrixItems[this.matrixItems.length - 1].position.x === 0 ? 'row' : 'column';
-      return `${groupType}`;
+      if (this.matrixItems[0].position.x === this.matrixItems[1].position.x) {
+        return `column-${this.matrixItems[0].position.x + 1}`;
+      }
+      return `row-${this.matrixItems[0].position.y + 1}`;
     },
     ...mapState({
       bpm: state => state.app.bpm,
