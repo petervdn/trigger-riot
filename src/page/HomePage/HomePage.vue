844eb003d9c22e7443e24f612ed5b47702019d76
Peter van der Noord <peterdunord@gmail.com>
2018-11-17 15:50:50 +0100

    Reposition main waveview (full width)

--- src/page/HomePage/HomePage.vue
+++ src/page/HomePage/HomePage.vue
@@ -6,25 +6,25 @@
 <template>
   <div>
     <Header />
+
     <div :class="$style.wrap">
+      <WaveView
+        :matrix-items="activeMatrixItems"
+        :initial-time-window="20"
+        :height="70"
+        :wave-margin="20"
+        :show-controls="true"
+      />
+      <div :class="$style.columns">
+        <div :class="$style.matrix">
+          <ModeSelector />
+          <Matrix />
+        </div>
 
-      <div :class="$style.matrix">
-        <WaveView
-          :matrix-items="activeMatrixItems"
-          :initial-time-window="20"
-          :height="70"
-          :wave-margin="20"
-          :show-controls="true"
-        />
-        <!--<input v-model.number="bpm">-->
-        <ModeSelector />
-        <Matrix />
+        <div :class="$style.sidebar">
+          <Sidebar />
+        </div>
       </div>
-
-      <div :class="$style.sidebar">
-        <Sidebar />
-      </div>
-
     </div>
   </div>
 </template>
